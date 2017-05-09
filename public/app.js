32f49d3820087f86bfdfe89d3e062544e214ef0d
Tom Bridgeman <TBridgeman78@gmail.com>
2017-05-09 11:22:38 -0700

    init commit

---
+++ public/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.all('/', function(req, res) {			    
+    res.sendFile('index.html', { root: 'public'});
+});
+
+app.listen(8000, function () {
+  console.log("I'm listening...");
+})
